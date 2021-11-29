63f8adf8087825d47d9392206d78cddc6dfe2e2d
Dimitri Rybak <dima_rybak@yahoo.de>
2021-11-29 12:00:44 +0100

    changed docker port

--- Dockerfile
+++ Dockerfile
@@ -19,4 +19,5 @@ RUN pip install -r requirements.txt
 # For environments with multiple CPU cores, increase the number of workers
 # to be equal to the cores available.
 # Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 remove_background:app
+# CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 remove_background:app
+CMD exec gunicorn --bind :8080 --workers 1 --threads 8 --timeout 0 remove_background:app
---
+++ test.py
@@ -0,0 +1,26 @@
+import requests
+from PIL import Image
+import io
+import os
+import json
+
+#resp = requests.post("http://localhost:5000", files={'file': open('ServicePic/NarutoHokage.png', 'rb')})
+ref_size = 500
+
+url = "http://localhost:5000"
+data = {'ref-size': ref_size,
+        'greyscale': 'Ye' } # noly "Yes" will give the geyscale picture
+filename = 'ServicePic/Naruto.png'
+file_output_name = "Naruto{0}.png".format(ref_size)
+
+files = [
+    ('file', open(filename, 'rb')),
+    ('data', ('data', json.dumps(data), 'application/json')),
+]
+
+resp = requests.post(url, files=files)
+print(resp.status_code)
+#print(resp.content)
+#print(resp.json())
+
+Image.open(io.BytesIO(resp.content)).save(os.path.join('ServicePicOutput', file_output_name))
